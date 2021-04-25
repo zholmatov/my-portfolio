279925f5fe733ccfa12816cfef03399860c29afe
Daniiar Zholmatov <daniyar.zholmatov@gmail.com>
2021-04-25 20:49:17 +0900

    adding in sanity studio

---
+++ src/client.js
@@ -0,0 +1,10 @@
+import sanityClient from 'sanity/client'
+//to connect react with sanity
+// thus we need project id for this
+
+export default sanityClient({
+    projectId: "yorh10fv",
+    // you can find this project ID in two places, 1: sanityClient.json -> api -> projectID
+    // 2: go to manage.sanity.io -> to your project -> PROJECT ID is there
+    dataset: "production"
+})
